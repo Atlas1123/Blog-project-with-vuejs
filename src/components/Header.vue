8ae681aaa7ce703c483668a02ba5760b71229588
Atlas1123 <atlas20011123@gmail.com>
2022-12-15 23:21:10 -0800

    initial add

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page-container">
+    <div class="header">
+      <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="125" height="125" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <h2>Welcome to my Vue Project.</h2>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
